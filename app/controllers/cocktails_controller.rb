400c34670e57a04a3c1a90c99f5313ba88a1dea5
Nachiket Pusalkar <nachiket87@gmail.com>
2020-08-13 16:18:50 -0400

    hve to add destroy

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,9 +13,13 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
 
-    redirect_to cocktails_path
+    end
+
   end
 
   private
